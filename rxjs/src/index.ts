e4b534648edeff7d6e29d3c92b1ba1eda17e41cb
Bruno Da Silva Costa <brdasilvac@engdb.infra>
2022-10-29 16:11:56 -0300

    delay mouse

--- rxjs/src/index.ts
+++ rxjs/src/index.ts
@@ -1,12 +1,28 @@
-import { from } from 'rxjs';
+import { map, filter, delay } from 'rxjs/operators';
+import { fromEvent } from 'rxjs';
 
-let numbers = [1, 5, 10];
-let source = from(numbers);
-
-function component() {
-    source.subscribe({
-        next: (x) => console.log(x)
-    });
+interface mouseTrack {
+    x: number;
+    y: number;
 }
 
-component();
+let circle = document.getElementById('circle');
+let source = 
+    fromEvent(document, 'mousemove')
+        .pipe(
+            map((e: MouseEvent) => { return { x: e.clientX, y: e.clientY } }),
+            filter((value: mouseTrack) => value.x < 500),
+            delay(300)
+        );
+
+function onNext(value: mouseTrack) {
+    console.log(value);
+    circle.style.left = `${value.x}px`;
+    circle.style.top = `${value.y}px`;
+}
+
+source.subscribe({
+    next: (value: MouseEvent) => onNext(value),
+    error: (e: Error) => console.log(e),
+    complete: () => console.log('Complete!')
+});
