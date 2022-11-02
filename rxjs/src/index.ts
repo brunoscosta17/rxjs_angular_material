53d54985b2e944d422d835a19d718ec890d2e47c
Bruno Da Silva Costa <brdasilvac@engdb.infra>
2022-11-02 10:35:08 -0300

    finalizado

--- rxjs/src/index.ts
+++ rxjs/src/index.ts
@@ -1,28 +1,46 @@
-import { map, filter, delay } from 'rxjs/operators';
-import { fromEvent } from 'rxjs';
+import { fromEvent, Observable } from 'rxjs';
+import { map, filter, delay, switchMap, retry } from 'rxjs/operators';
 
-interface mouseTrack {
-    x: number;
-    y: number;
+interface IMovie {
+    title: string;
 }
 
-let circle = document.getElementById('circle');
-let source = 
-    fromEvent(document, 'mousemove')
-        .pipe(
-            map((e: MouseEvent) => { return { x: e.clientX, y: e.clientY } }),
-            filter((value: mouseTrack) => value.x < 500),
-            delay(300)
-        );
+let button = document.getElementById('button');
+let output = document.getElementById('output');
+let click = fromEvent(button, 'click');
 
-function onNext(value: mouseTrack) {
-    console.log(value);
-    circle.style.left = `${value.x}px`;
-    circle.style.top = `${value.y}px`;
+function load(url: string): Observable<any> {
+    return new Observable(subscriber => {
+        let xhr = new XMLHttpRequest();
+        output.innerHTML = '';
+        xhr.addEventListener('load', () => {
+            if (xhr.status === 200) {
+                let movies = JSON.parse(xhr.responseText);
+                subscriber.next(movies);
+                subscriber.complete();
+            } else {
+                subscriber.error(xhr.statusText);
+            }
+        });
+        xhr.open('GET', url);
+        xhr.send();
+    }).pipe(
+        retry({ count: 3, delay: 1000 })
+    );
 }
 
-source.subscribe({
-    next: (value: MouseEvent) => onNext(value),
-    error: (e: Error) => console.log(e),
-    complete: () => console.log('Complete!')
-});
+function renderMovie(movies: IMovie[]) {
+    movies.forEach((movie: IMovie) => {
+        let div = document.createElement('div');
+        div.innerText = movie.title;
+        output.appendChild(div);
+    });
+}
+
+click.pipe(
+    switchMap(() => load('./src/movies.json')))
+    .subscribe({
+        next: renderMovie,
+        error: (e: Error) => console.log(`Error: ${e}`),
+        complete: () => console.log('Complete!')
+    });
