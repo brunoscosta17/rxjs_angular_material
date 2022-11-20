d8d8bb2192ac0dbb06a21d7c5eb7be63fe01a544
Bruno Da Silva Costa <brdasilvac@engdb.infra>
2022-11-20 08:02:24 -0300

    Icons and buttons

--- angular-material/src/app/app.module.ts
+++ angular-material/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from './material/material.module';
+import { ButtonsIconsComponent } from './buttons-icons/buttons-icons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonsIconsComponent
   ],
   imports: [
     BrowserModule,
