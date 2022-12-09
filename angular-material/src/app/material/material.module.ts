de9acae572341c31b67c3edd1106d3cb009fdfef
Bruno Da Silva Costa <brdasilvac@engdb.infra>
2022-12-09 16:20:04 -0300

    formulário sistema produtor

--- angular-material/src/app/material/material.module.ts
+++ angular-material/src/app/material/material.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatInputModule } from '@angular/material/input';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [],
@@ -16,7 +17,8 @@ import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
     MatIconModule,
     MatButtonModule,
     MatSidenavModule,
-    MatInputModule
+    MatInputModule,
+    MatListModule
   ],
   providers: [
     { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } }
